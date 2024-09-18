b5206a7f235517168c14ec336b9b8956681916e5
paul-caayao <paulcaayao@gmail.co>
2024-09-18 14:44:20 +0800

    Change headline

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.get("/", (req, res) => {
-  res.send("Hello from Nodes API");
+  res.send("Hello I am Paul Caayao");
 });
 
 // routes
